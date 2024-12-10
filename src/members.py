1f8e44981bfce6a4a2f68bed829b6ace3d5c75e7
Ralph Maron Eda <edaralphmaron@gmail.com>
2024-12-10 11:14:46 +0800

    generating members dynamically and only included those who vote on poll i made yesterday.

--- src/members.py
+++ src/members.py
@@ -20,28 +20,29 @@ class MemberFrame:
             font=FONT
         )
         members_label.grid(row=0, column=0, pady=10)
-        member_button1 = tk.Button(
-            master=frame_members,
-            text="Ralph Maron Eda",
-            bg=FOREGROUND,
-            fg=BACKGROUND,
-            font=FONT
-        )
-        member_button2 = tk.Button(
-            master=frame_members,
-            text="Triesha Mae Olunan",
-            bg=FOREGROUND,
-            fg=BACKGROUND,
-            font=FONT
-        )
-        member_button3 = tk.Button(
-            master=frame_members,
-            text="Jezlyn Cabbab",
-            bg=FOREGROUND,
-            fg=BACKGROUND,
-            font=FONT
-        )
 
-        member_button1.grid(row=1, column=0, pady=5)
-        member_button2.grid(row=2, column=0, pady=5)
-        member_button3.grid(row=3, column=0, pady=5)
+        members = [
+            'Ralph Maron Eda',
+            'Angel Apay',
+            'Marylitte Capistrano',
+            'Kurt',
+            'Triesha Mae',
+            'Shanlei',
+            'Glenn Kylle',
+            'Ariel',
+            'Elisha',
+            'Joanna',
+            'Neal',
+            'Joseph',
+            'Hannah',
+            'Jamaica'
+        ]
+        for index, member in enumerate(members):
+            member_label = tk.Label(
+                master=frame_members,
+                text=member,
+                bg=BACKGROUND,
+                fg=FOREGROUND,
+                font=FONT
+            )
+            member_label.grid(row=index + 1, column=0, pady=5)  # Start from row 1 to row 4
