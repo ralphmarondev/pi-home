e8370aeef5b31e3abbd9ce93caee092744108bef
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-01-15 06:41:56 +0800

    added some notes on constants.py

--- app/constants.py
+++ app/constants.py
@@ -1,3 +1,14 @@
+"""
+NOTES:
+    DOOR SERVO FORMULA:
+        7.5 = 2.5 + (90degrees/18.0) 0deg = 2.5%, 18.0 = 180 deg = 12.5%
+
+    SERVO CONFIGURATION:
+        Red [VCC] 5v or 3.3v on rpi
+        Brown/Black [GND] ground pin on rpi
+        Yellow/Orange [Signal] -> GPIO17
+"""
+
 # pins for led
 BUTTON_LED_PINS = {
     'button1': 17,
